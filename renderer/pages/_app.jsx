7624a955b267aee03df9c83d6457e99d6e2c37a0
Spirit <66965600+SpiritLetsPlays@users.noreply.github.com>
2022-11-25 18:46:23 +0100

    Update _app.jsx

--- renderer/pages/_app.jsx
+++ renderer/pages/_app.jsx
@@ -86,8 +86,10 @@ function MyApp({ Component, pageProps }) {
   var setup = router.pathname.split("/").pop() === "setup";
   var migrate = router.pathname.split("/").pop() === "migration";
 
-  router.events.on("routeChangeComplete", () => {
-    setIsOverlayShown(false);
+  React.useEffect(() => {
+    router.events.on("routeChangeComplete", () => {
+      setIsOverlayShown(false);
+    });
   });
 
   React.useEffect(async () => {
